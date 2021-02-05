21ba607f7f0e8535bff4aeb85152919ae4453cd8
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-05 15:49:08 -0300

    Builded!

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sourcerer0/deliv",
     packages=setuptools.find_packages(),
+    keywords="delivery scraper",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License 2.0",
