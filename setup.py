23e379364f5a964f8f5be8a1943fa1ca605988ec
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-15 18:03:27 -0300

    1.1.0-b.1 -> 1.1.0-b.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="deliv",
-    version="1.1.0-b.1",
+    version="1.1.0-b.2",
     author="Lo Han",
     author_email="lohan.uchsa@protonmail.com",
     description="Delivery service scraper",
